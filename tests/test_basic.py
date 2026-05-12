92108584b48cc9c77b45e7b1afecd2b68418f0a5
nourhene <benothmennourhen8@gmail.com>
2026-05-12 08:22:45 +0100

    chore: add tests directory structure

---
+++ tests/test_basic.py
@@ -0,0 +1,8 @@
+def test_environment():
+    """Vérifie que l'environnement est correctement configuré"""
+    assert True
+
+def test_imports():
+    """Vérifie que les librairies s'importent"""
+    import pandas, numpy, sklearn, fastapi, uvicorn
+    assert True
